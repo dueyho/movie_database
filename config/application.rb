e3e423811a3a469b96f3af8667d84973c30782cd
Drew Seminara & Due Ho <student@epicodus.com>
2014-04-04 17:14:40 -0700

    full functionality with many to many join between genre and movies

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -24,6 +25,7 @@ module ToSee
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     config.action_controller.permit_all_parameters = true
   end
 end
